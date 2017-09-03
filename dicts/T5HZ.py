4acd077379fcccb88e1ece6647045ae69dd34941
Kevin Pedro <kpedro88@gmail.com>
2017-09-02 22:27:22 -0500

    add T5HZ diagram

---
+++ dicts/T5HZ.py
@@ -0,0 +1,16 @@
+from imports import *
+
+components = getComponents("T5qqqqVV",copy=True)
+
+components["qua1a"]["label"]["name"] = qbar
+components["qua1b"]["label"]["name"] = qua
+components["chi1c"]["label"]["name"] = chi20
+components["part1d"]["label"]["name"] = Z0
+components["part1e"]["label"]["name"] = chi10
+
+components["qua2a"]["label"]["name"] = qua
+components["qua2b"]["label"]["name"] = qbar
+components["chi2c"]["label"]["name"] = chi20
+components["part2d"]["label"]["name"] = hig
+components["part2d"]["type"] = Scalar
+components["part2e"]["label"]["name"] = chi10
